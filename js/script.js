c1b8d19a9bf31fb3c42ab09db21b0c4fc2b95e77
Olga-Zhuleva <zhuleva_o@list.ru>
2023-03-20 11:27:46 +0300

    changes swiper

--- js/script.js
+++ js/script.js
@@ -11,3 +11,66 @@ new Swiper('.swiper', {
         dynamicBullets:true,
     },
 });
+
+(async () => {
+    let user = {
+      name: 'John',
+      surname: 'Smith'
+    };
+    
+    let response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(user)
+    });
+        if (!response.ok) {
+            throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response}`);
+        } 
+        return await response.json();
+    
+    let result = await response.json();
+    alert(result.message);
+    })()
+//'https://jsonplaceholder.typicode.com/users'
+
+// fetch('http://localhost:3000/entity')
+// .then(data => {
+//         console.log(data);
+//         return data.text();
+// })
+// .then(data => {
+//     console.log(data);
+// });
+
+// fetch('http://localhost:3000/entity', {
+//     method: 'POST', 
+//     headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded',
+
+//     },
+//     body: 'hello',
+// })
+//     .then(response => response.text())
+//     .then(response => {
+//         console.log(response);
+//     })
+
+// // const response = await fetch(url, {
+// //     method: 'POST',
+// //     body: database,
+// // });
+// //     if (!response.ok) {
+// //         throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response}`);
+
+// //     }
+// //     return await response.json();
+
+// //     const form = () => {
+// //     const formData = document.querySelector('.form-data');
+        
+// //         const data = {
+            
+// //         }
+// //     }
