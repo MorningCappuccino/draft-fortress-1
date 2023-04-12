949b56ecf086bb2f8e4ce411de0b6030662ca190
MorningCappuccino <knockknockwakeup@gmail.com>
2023-04-12 14:01:55 +0300

    react: husky alive!

--- client3/src/components/cyber_add-review.tsx
+++ client3/src/components/cyber_add-review.tsx
@@ -1,11 +1,28 @@
-
 export default function CyberAddReview() {
-    return <div className="pt-20 pl-5 pb-10 flex justify-between">
-        <textarea className="border border-aqua border-b-2 bg-black p-4 placeholder-aqua text-white text-lg outline-none"
-            name="" id="" cols="44" rows="4" placeholder="Type our review..."></textarea>
-        <div className="flex flex-col justify-between items-end">
-            <input className="bg-black border-aqua border-b-2 placeholder-aqua text-lg outline-none" style={{width: 330, lineHeight: '40px'}} placeholder="Signature..." type="text" />
-            <button className="text-aqua uppercase text-2xl bg-button bg-no-repeat" style={{height: 62, width: 264}}>Submit</button>
-        </div>
+  return (
+    <div className="pt-20 pl-5 pb-10 flex justify-between text-lg">
+      <textarea
+        className="border border-aqua border-b-2 bg-black p-4 placeholder-aqua text-white text-lg outline-none"
+        name=""
+        id=""
+        cols="44"
+        rows="4"
+        placeholder="Type our review..."
+      ></textarea>
+      <div className="flex flex-col justify-between items-end">
+        <input
+          className="bg-black border-aqua border-b-2 placeholder-aqua text-lg outline-none"
+          style={{ width: 330, lineHeight: "40px" }}
+          placeholder="Signature..."
+          type="text"
+        />
+        <button
+          className="text-aqua uppercase text-2xl bg-button bg-no-repeat"
+          style={{ height: 62, width: 264 }}
+        >
+          Submit
+        </button>
+      </div>
     </div>
+  );
 }
